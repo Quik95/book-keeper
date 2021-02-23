ba221c49b1e42398a11c927390d6e630d679f404
Sebastian Bartoszewicz <quikstyletv@gmail.com>
2021-02-23 16:46:54 +0100

    Use log.Fatal instead of fmt.Println

--- cmd/keeper.go
+++ cmd/keeper.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"path"
@@ -16,8 +15,7 @@ func main() {
 	if len(os.Args) == 2 {
 		location = os.Args[1]
 	} else {
-		fmt.Println("Please provide the database location")
-		os.Exit(1)
+		log.Fatal("Please provide the database location")
 	}
 
 	// when given a directory path automically set the database name
